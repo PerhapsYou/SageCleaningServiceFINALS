d67233c0721ef70f0925144e0f1196411ef01d4a
ejionaut <78855012+ejionaut@users.noreply.github.com>
2022-05-10 21:29:04 +0800

    Added UI calling to main
    Still working on the log-in portion. If someone else want to work on that feel free

--- src/Controler/JavaPB.java
+++ src/Controler/JavaPB.java
@@ -3,24 +3,25 @@ package Controler;
 import java.util.ArrayList;
 
 public class JavaPB {
+    // Set of arraylist...
+    // userInfo holds the current users details
+    public static ArrayList<String> userInfo = new ArrayList<String>();
+    // Data =  Holds other necessary details in job orders (service,location,sched,time,payment);
+    public static ArrayList<String> data = new ArrayList<String>();
+    public static Boolean loggedIn = false;
+    
     public static void main(String[] args) {
-        // Set of arraylist...
         DataPB.setCon();
 
-        Boolean loggedIn = false;
-
-        while(!(loggedIn)){
-            // Working on it.. once login then return true.
-            /**
-             * if(View.UserLogin.startUserLogin()){
-             *                 loggedIn = true;
-             *             }
-             */
+        UserLogin.startUserLogin(); // Working on Username and Password log-in
+        
+        while(loggedIn){
+            String accountType = userInfo.get(5);
+            if(accountType.equals("Employee")){
+                MainWorkerInterface.pendingRequestWorker();
+            } else {
+                ClientJobRequest.jobRequest();
+            }
         }
-
-        /**
-         *  If AccountType = Customer then clientJobRequest
-         *      Else WorkerInterface..
-         */
     }
 }
